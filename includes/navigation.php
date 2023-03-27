12468851e5e973092c1c677481e48c44f24fc5bd
Czooi <126080176+Czooi@users.noreply.github.com>
2023-03-27 17:22:34 +0800

    Create navigation.php

---
+++ includes/navigation.php
@@ -0,0 +1,11 @@
+<nav class="topNavigation">
+<ul>
+  <li><a href="http://localhost/PiE">Home</a></li>
+  <li><a href="http://localhost/PiE/about">About</a></li>
+  <li><a href="http://localhost/PiE/expressway">Pan Island Expressway</a></li>
+  <li><a href="http://localhost/PiE/project">Project Updates</a></li>
+  <li><a href="http://localhost/PiE/traffic">Traffic Announcements</a></li>
+  <li><a href="http://localhost/PiE/Contact">Contact Us</a></li>
+</ul>
+
+</nav>
