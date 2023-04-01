7eab6c595f157116935c93d6307408a5261009f6
Czooi <126080176+Czooi@users.noreply.github.com>
2023-04-02 00:15:43 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,21 @@
 <header class="pageHeader">
-<h1>Pan Island Expressway Berhad</h1>
+  <div class="header_container">
 
+    <div class="header_center">
+      <a href="login.html" class="h_item"><p>Free Shipping For All Malaysian Rewards Member. Join Us Now.</p></a> 
+    </div>
+
+    <div class="header_right">
+      <div class="header_item2">
+        <a href="#" class="h_item" id="register">Register</a>
+      </div>
+      <div class="header_item2">
+        <a href="#" class="h_item">|</a>
+      </div>
+      <div class="header_item2">
+        <a href="#" class="h_item" id="li">Log In</a>
+      </div>
+    </div>
+    
+   </div>
 </header>
