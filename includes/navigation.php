a82370d1b7622a53c12cc07f5ad9ace32305c02e
Czooi <126080176+Czooi@users.noreply.github.com>
2023-04-02 00:15:46 +0800

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,11 +1,81 @@
-<nav class="topNavigation">
-<ul>
-  <li><a href="http://localhost/PiE">Home</a></li>
-  <li><a href="http://localhost/PiE/about">About</a></li>
-  <li><a href="http://localhost/PiE/expressway">Pan Island Expressway</a></li>
-  <li><a href="http://localhost/PiE/project">Project Updates</a></li>
-  <li><a href="http://localhost/PiE/traffic">Traffic Announcements</a></li>
-  <li><a href="http://localhost/PiE/Contact">Contact Us</a></li>
-</ul>
-
-</nav>
+<nav class="navbar">
+       <div class="navbar_container">
+          <div class="navbar_logo">
+           <div class="navbar_item1">
+             <a href="index.html" id="navbar__logo"><img href="index.html" src="images/logo/Move_logo_withoutslogan.png" alt="logo" class="logo" height ="42px" width="130px"></a>
+           </div>
+         </div>
+          <div class="navbar_categories">
+           
+           <div class="navbar_item2">
+             <div class="dropdown">
+               <button class="n_item_title" >New & Latest</button>
+             </div>
+             <div class="dropdown-content">
+               <a href="#" class="n_item">Cloths</a>
+               <a href="#" class="n_item">Footwear</a>
+               <a href="#" class="n_item">Accessories</a>
+             </div>
+           </div>
+           
+           <div class="navbar_item2">
+             <div class="dropdown">
+               <button href="#" class="n_item_title" >Clothing</button>
+             </div>  
+             <div class="dropdown-content">
+               <a href="#" class="n_item">T-Shirts</a>
+               <a href="#" class="n_item">Hoodies</a>
+               <a href="#" class="n_item">Jackets</a>
+               <a href="#" class="n_item">Trouser & Tights</a>
+               <a href="#" class="n_item">Socks</a>
+             </div>
+           </div>
+            <div class="navbar_item2">
+             <div class="dropdown">
+               <button href="#" class="n_item_title" >Footwear</button>
+             </div>
+             <div class="dropdown-content">
+               <a href="#" class="n_item">Running</a>
+               <a href="#" class="n_item">Basketball</a>
+               <a href="#" class="n_item">Footbal</a>
+               <a href="#" class="n_item">Training & Gym</a>
+             </div>
+           </div>
+            <div class="navbar_item2">
+             <div class="dropdown">
+               <button href="#" class="n_item_title">Accessories</button>
+             </div>
+             <div class="dropdown-content">
+               <a href="#" class="n_item">Sports Accessories</a>
+               <a href="#" class="n_item">Bags</a>
+               <a href="#" class="n_item">Fashion</a>
+             </div>
+           </div>
+            <div class="navbar_item2">
+             <div class="dropdown">
+               <button href="#" class="n_item_title">Brands</button>
+             </div>
+             <div class="dropdown-content">
+               <a href="#" class="n_item">Nike</a>
+               <a href="#" class="n_item">Under Armour</a>
+               <a href="#" class="n_item">Jordan</a>
+               <a href="#" class="n_item">Adidas</a>
+             </div>
+           </div>
+           
+         </div>
+          <div class="navbar_icons">
+           <div class="navbar_item3">
+             <a href="login.php" id="login-page"><img src=images/icons/user.png  width="25px"></a>
+           </div>
+           <div class="navbar_item3">
+             <a href="wishlist.html" id="wishlist-page"><img src=images/icons/wishlist-icon.png  width="25px"></a>
+           </div>
+           <div class="navbar_item3">
+             <a href="add_to_cart.html" id="shopping-bag-page"><img src=images/icons/shopping-bag-icon.png  width="25px"></a>
+           </div>
+         </div>
+         
+           
+       </div>
+ </nav>
