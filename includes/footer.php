5f140cf12904f6a5eca6fada50183e5529bbe991
Czooi <126080176+Czooi@users.noreply.github.com>
2023-04-02 00:15:40 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,89 @@
-<footer class="pageFooter">
-&copy; 2016 Pan Island Expressway Berhad. All right reserved.
+<footer class="pageFooter1">
+    <div class="footer_container1">
+      <div class="footer1_left">
+        <div class="footer_subcontainer1">
+          <div class="footer_item">
+            <h5 class="f_item_title">Product</h5>
+          </div>
+          <div class="footer_item">
+            <a href="#" class="f_item"><p>Clothing</p></a>
+          </div>
+          <div class="footer_item">
+            <a href="#" class="f_item"><p>Footwear</p></a>
+          </div>
+          <div class="footer_item">
+            <a href="#" class="f_item"><p>Accessories</p></a>
+          </div>
+        </div>
+  
+        <div class="footer_subcontainer2">
+          <div class="footer_item">
+            <h5 class="f_item_title">Customer Care</h5>
+          </div>
+          <div class="footer_item">
+            <a href="#" class="f_item"><p>Contact Us</p></a>
+          </div>
+          <div class="footer_item">
+            <a href="#" class="f_item"><p>Live Chat</p></a>
+          </div>
+          <div class="footer_item">
+            <a href="#" class="f_item"><p>Email Support</p></a>
+          </div>
+          <div class="footer_item">
+            <a href="#" class="f_item"><p>FAQ</p></a>
+          </div>
+        </div>
+  
+        <div class="footer_subcontainer3">
+          <div class="footer_item">
+            <h5 class="f_item_title">About Move</h5>
+          </div>
+          <div class="footer_item">
+            <a href="#" class="f_item"><p>Company Info</p></a>
+          </div>
+          <div class="footer_item">
+            <a href="#" class="f_item"><p>Brand Identity</p></a>
+          </div>
+          <div class="footer_item">
+            <a href="#" class="f_item"><p>Events</p></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer1_right">
+        <div class="footer_subcontainer4">
+          <div class="footer_item4">
+            <a href="https://www.facebook.com/" id="facebook-icon"><img src=images/icons/facebook.png  width="30px"></a>
+          </div>
+          <div class="footer_item4">
+            <a href="https://www.instagram.com/" id="instagram-icon"><img src=images/icons/instagram.png  width="30px"></a>
+          </div>
+          <div class="footer_item4">
+            <a href="https://twitter.com/" id="twitter-icon"><img src=images/icons/twitter.png  width="30px"></a>
+          </div>
+          <div class="footer_item4">
+            <a href="https://www.tiktok.com/" id="tiktok-icon"><img src=images/icons/tiktok.png  width="30px"></a>
+          </div>
+          <div class="footer_item4">
+            <a href="https://www.youtube.com/" id="youtube-icon"><img src=images/icons/youtube.png  width="30px"></a>
+          </div>
+        </div>
+      </div>
+
+  </footer>
+<footer class="pageFooter2">
+  <div class="footer_container2">
+    <div class="footer2_left">
+      <p class="f2_item" >Visit our corporate website at <a href="#">www.move.com</a></p>
+      <span><p class="f2_item">Copyright &copy; 2023 Move Sports. All right reserved.</p></span>
+    </div>
+    <div class="footer2_right">
+      <p style="text-align: center;" class="f2_item">We accept the following payment methods</p>
+      <div class="f2_container">
+        
+        <img src=images/icons/payment_method.png  width="500px">
+        
+      </div>
+    </div>
+  </div>
 </footer>
