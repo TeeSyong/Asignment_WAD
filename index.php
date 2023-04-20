c913ef3b6d2fc29a0a6cb4f93aacd1ab99df0f1a
Elainee610 <elaine610.lau@gmail.com>
2023-04-21 01:18:02 +0800

    update index.php

--- index.php
+++ index.php
@@ -16,22 +16,47 @@
 <?php include('includes/navigation.php'); ?>
 
 <!--video -->
-<div id = "myVideo">
+<div id = "myVideo" controls>
     <video autoplay muted>
         <source src="images/home/banner_video.mp4" type="video/mp4">
     </video>
     <div id = "videoWords">
         <h1>MOVE</h1>
-        <h3>Move It Will Do It</h3>
-        <p></p><!---->
+        <h1>Move It Will Do It</h1>
+        <h1>COMING SOON</h1>
     </div>
 </div>
-<!--scroll down to start play, stop after play once-->
 
-<!--About-->
-<div>
-    <h1>About Us</h1>
-</div>
+<!--scroll down to start play-->
+<!-- <script>
+        // Get the video element and its offset top position from the top of the document
+    const video = document.getElementById('myVideo');
+    const videoOffsetTop = video.offsetTop;
+
+    // Function to check if the video is in the viewport
+    function isInViewport() {
+        const scrollTop = window.pageYOffset;
+        const videoTop = videoOffsetTop - scrollTop;
+        const videoBottom = videoTop + video.offsetHeight;
+        const viewportTop = scrollTop + window.innerHeight;
+        const viewportBottom = scrollTop;
+
+        return (videoTop < viewportTop) && (videoBottom > viewportBottom);
+    }
+
+    // Function to play the video when it's in the viewport
+    function playVideoOnScroll() {
+    if (isInViewport()) {
+        video.play();
+        // Remove the scroll event listener to prevent the video from playing multiple times
+        window.removeEventListener('scroll', playVideoOnScroll());
+    }
+    }
+
+    // Add the scroll event listener to trigger the video to play
+    window.addEventListener('scroll', playVideoOnScroll());
+
+</script> -->
 
 <!--shop by category-->
 <h1>Our Products</h1>
@@ -58,73 +83,76 @@
 
 <!-- logos-->
 <div class="frame">
-<div class="slider">
-    <div class="slide-track">
-        <div class="slide">
-            <img class="sliderImg" src="images/home/Adidas-logo.png" alt="Adidas">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/NIKE-logo.png" alt="Nike">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/PUMA-logo.png" alt="Puma">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/Reebok-logo.png" alt="Reebok">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/underArmour-logo.png" alt="Under Armour">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/Jordan-logo.png" alt="Jordan">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/Vans-logo.png" alt="Vans">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/New_Balance-logo.png" alt="New Balance">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/Asics-logo.png" alt="Asics">
-        </div>
+    <div class="slider">
+        <div class="slide-track">
+            <div class="slide">
+                <img class="sliderImg" src="images/home/Adidas-logo.png" alt="Adidas">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/NIKE-logo.png" alt="Nike">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/PUMA-logo.png" alt="Puma">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/Reebok-logo.png" alt="Reebok">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/underArmour-logo.png" alt="Under Armour">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/Jordan-logo.png" alt="Jordan">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/Vans-logo.png" alt="Vans">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/New_Balance-logo.png" alt="New Balance">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/Asics-logo.png" alt="Asics">
+            </div>
 
-            <!-- Another 9 Images (DOUBLED) -->
+                <!-- Another 9 Images (DOUBLED) -->
 
-        <div class="slide">
-            <img class="sliderImg" src="images/home/Adidas-logo.png" alt="Adidas">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/NIKE-logo.png" alt="Nike">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/PUMA-logo.png" alt="Puma">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/Reebok-logo.png" alt="Reebok">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/underArmour-logo.png" alt="Under Armour">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/Jordan-logo.png" alt="Jordan">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/Vans-logo.png" alt="Vans">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/New_Balance-logo.png" alt="New Balance">
-        </div>
-        <div class="slide">
-            <img class="sliderImg" src="images/home/Asics-logo.png" alt="Asics">
+            <div class="slide">
+                <img class="sliderImg" src="images/home/Adidas-logo.png" alt="Adidas">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/NIKE-logo.png" alt="Nike">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/PUMA-logo.png" alt="Puma">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/Reebok-logo.png" alt="Reebok">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/underArmour-logo.png" alt="Under Armour">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/Jordan-logo.png" alt="Jordan">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/Vans-logo.png" alt="Vans">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/New_Balance-logo.png" alt="New Balance">
+            </div>
+            <div class="slide">
+                <img class="sliderImg" src="images/home/Asics-logo.png" alt="Asics">
+            </div>
+
         </div>
 
     </div>
-
 </div>
-    </div>
+
+
+<!--search-->
+
 
 <!--top picks-->
-
 <h1>Top Picks</h1>
 <div class="flex-container">
     <div>  
@@ -147,17 +175,17 @@
     </div>
 </div>
 
-<!--search-->
-
 <!--sign up/login-->
 <div id="LoginSignUpHome" style="background-image: url('images/home/loginSignupBgr.jpg');">
-    <h3 id="LoginSignUpWords">Sign up now to get early access to latest product</h3>
+    <h3 id="LoginSignUpWords">Sign up now to get early access to the latest product</h3>
     <div class="buttonFlex">
         <button type="button" class="button hoverButton" onclick="window.open('login.php','_self')">Login</button>
         <button type="button" class="button hoverButton" onclick="window.open('signup.php', '_self')">Sign Up</button>
     </div>
 </div>
 
+<button onclick="topFunction()" id="topBtn">Back to Top</button>
+<script src="js/home.js"></script>
 
 <!--footer-->
 <?php include('includes/footer.php'); ?>
