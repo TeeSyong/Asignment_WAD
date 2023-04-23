4e7379264f4282dc6dda63e2f784ee9b8b9725a2
Czooi <126080176+Czooi@users.noreply.github.com>
2023-04-23 13:06:17 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -13,38 +13,43 @@
     <?php include('includes/header.php'); ?>
     <?php include('includes/navigation.php'); ?>
 
-    <div class="signupform">
-      <div class="imgcontainer">
-        <img src="images/logo/Move_logo.png" alt="logo" class="logo" width="350px">
+    <div id="wrapper_container" style="background-image: url('images/background/background_sports.jpg');">
+
+      <div class="wrapper">
+        <center>
+          <div class="imgcontainer">
+            <img src="images/logo/MoveLogo.png" alt="logo" class="logo" width="250px">
+          </div>
+        </center>
+
+        <h2 style="text-align: center; padding:15px 0 0 0;"><b>Register Your Details</b> </h2>
+        <form class="signupform">
+          <label for="fname" ><b>First Name</b></label> 
+          <input type="text" id="fname" placeholder="First Name" required>
+          
+          <label for="lname" ><b>Last Name</b></label> 
+          <input type="text" id="lname" placeholder="Last Name" required>
+          
+          <label for="email"><b>Email</b></label>
+          <input type="text" placeholder="Email Address" name="email" required>
+
+          <label for="phone"><b>Phone Number</b></label>
+          <input type="text" placeholder="Phone Number" name="phone" required> 
+    
+          <label for="password"><b>Password</b></label>
+          <input type="text" id="password" placeholder="Password" name="password" required>
+
+          <label for="password"><b>Confirm Password</b></label>
+          <input type="text" id="password" placeholder="Confirm Password" name="password" required>
+
+          <input type="checkbox" required>  I accept the <a href="#" class="link">Terms of Use</a> & <a href="#" class="link">Privacy Policy</a>
+          <br><br>
+
+          <input type="submit" value="Sign Up"><br><br>
+          
+          <p style="text-align: center;">Already have an account? <a href="login.php" class="link">Login here</a></p>
+        </form>
       </div>
-
-      <h2 style="text-align: center; padding:15px 0 0 0;"><b>Register Your Details</b> </h2>
-      <form class="signupform">
-        <label for="fname" ><b>First Name</b></label> 
-        <input type="text" id="fname" placeholder="First Name" required>
-        
-        <label for="lname" ><b>Last Name</b></label> 
-        <input type="text" id="lname" placeholder="Last Name" required>
-        
-        <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Email" name="email" required>
-
-        <label for="phone"><b>Phone Number</b></label>
-        <input type="text" placeholder="Phone Number" name="phone" required> 
-  
-        <label for="password"><b>Password</b></label>
-        <input type="text" id="password" placeholder="Password" name="password" required>
-
-        <label for="password"><b>Confirm Password</b></label>
-        <input type="text" id="password" placeholder="Confirm Password" name="password" required>
-
-        <input type="checkbox" required>  I accept the <a href="#" class="link">Terms of Use</a> & <a href="#" class="link">Privacy Policy</a>
-        <br><br>
-
-        <input type="submit" value="Sign Up"><br><br>
-        
-        <p style="text-align: center;">Already have an account? <a href="login.php" class="link">Login here</a></p>
-      </form>
     </div>
     <?php include('includes/footer.php'); ?>
   </body>
