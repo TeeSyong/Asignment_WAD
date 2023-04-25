76146b1227012beb7502d085a9bf7e280d88e626
TszenYong <tszenyong@gmail.com>
2023-04-25 23:47:36 +0800

    Minor Changes

--- cart/payment.php
+++ cart/payment.php
@@ -13,67 +13,6 @@
         require "../env.php";
         include('../includes/header.php');
         include('../includes/navigation.php');
-        
-        
-        echo "
-        <div>
-            <h1>Shopping Cart</h1><hr>    
-        </div>
-        ";
-        // establish connection
-        $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-
-        $query = "SELECT * FROM cart ORDER BY id ASC";
-
-        $result = mysqli_query($conn, $query);
-
-        if(mysqli_num_rows($result)>0){
-            echo "
-                <table>
-                <tr>
-                    <td> <button value='Remove all' id='remove_button'> Remove All </button> </td> 
-                </tr>
-            ";
-
-            while($row = mysqli_fetch_assoc($result)){
-                echo "
-                    <tr>
-                        <td rowspan='2'><input type='checkbox' id='checkbox'> </td>
-                        <td rowspan='2'>Img</td>
-                        <td>{$row['name']}</td>                            
-                        <td rowspan='2'>{$row['qtt']}</td>
-                        <td rowspan='2'>RM{$row['price']}</td>
-                    </tr>
-                    <tr>   
-                        <td>{$row['description']}</td>                 
-                    </tr>
-                    </hr>
-                ";
-            }
-
-            echo "
-                </table>
-            ";
-        }
-        
-        mysqli_close($conn);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
         $nameErr=$emailErr=$addErr=$cityErr=$stateErr=$postcodeErr="";
