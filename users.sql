6b80b9bf8aa17772dd3086f31b24f0d4c1d58ca7
Elainee610 <elaine610.lau@gmail.com>
2023-04-25 11:22:43 +0800

    update cart page (added database and created payment form)

--- users.sql
+++ users.sql
@@ -1,4 +1,6 @@
-//database --> move
+CREATE DATABASE IF NOT EXISTS move_database COLLATE utf8mb4_general_ci;
+
+USE move_database;
 
 CREATE TABLE users(
     id int(11) AUTO_INCREMENT,
@@ -10,6 +12,23 @@ CREATE TABLE users(
     cpassword varchar(255),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE checkout{
+    id int(11) AUTO_INCREMENT NOT NULL,
+    name varchar(100),
+    email varchar(190),
+    address varchar(255),
+    city varchar(50),
+    state varchar(50),
+    postcode varchar(10),
+    hp varchar(20),
+    paymentType varchar(20),
+    cardName varchar(100) NULL,
+    cardNum varchar(20) NULL,
+    exp varchar(10) NULL,
+    cvv varchar(5) NULL,
+    PRIMARY KEY(id)
+}
 
 
     
