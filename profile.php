6cf0344c2c2fbc0145ae851ce36ca82fa6a6a8c3
Allex Yeo <127025074+AllexYeo@users.noreply.github.com>
2023-04-28 02:36:21 +0800

    profile.php

--- profile.php
+++ profile.php
@@ -4,23 +4,16 @@
 <head>
     <title>Profile Page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel='stylesheet' href='css/profile.css'>
+    <link rel='stylesheet' type="text/css" href='css/profile.css'>
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/navigation.css">
     <link rel="stylesheet" href="css/footer.css">
-<<<<<<< Updated upstream
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
-=======
->>>>>>> Stashed changes
 </head>
 
 <body>
-<?php include('includes/header.php'); ?>
-    <?php include('includes/navigation.php');?>
+    <?php include('includes/header.php'); ?>
+    <?php include('includes/navigation.php'); ?>
     <section class="profile" style="background-image: url('images/background/profilebackground.jpg');">
         <div class="profileHeader">
         </div>
