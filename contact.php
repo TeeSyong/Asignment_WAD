1e54c9be2f681a29d776878360eeedf279583626
Elainee610 <elaine610.lau@gmail.com>
2023-04-28 10:30:01 +0800

    contact database and connect link for footer

--- contact.php
+++ contact.php
@@ -31,32 +31,59 @@
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $isValid = true;
 
         if (empty(trim($_POST["name"]))) {
             $nameErr = "Please fill in your name.";
+            $isValid = false;
         } else {
             $name = input_data($_POST["name"]);
             if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
                 $nameErr = "Only alphabets and white space are allowed";
+                $isValid = false;
             }
         }
 
         if (empty(trim($_POST["email"]))) {
             $emailErr = "Email is required";
+            $isValid = false;
         } else {
             $email = input_data($_POST["email"]);
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 $emailErr = "Invalid email format";
+                $isValid = false;
             }
         }
 
         if (trim($_POST['message'] == "")) {
             $messageErr = 'Please enter a description of your subject.<br/><br/>';
+            $isValid = false;
         } else {
             $message = input_data($_POST['message']);
         }
 
+        //save to database
+        if($isValid) {
+            $conn = mysqli_connect("localhost","root","","move_database");
+            if(!$conn){
+                die("Connection Error".mysqli_connect_error($conn));
+            }
 
+            $query = "INSERT INTO contact(name,email,message) VALUES (?,?,?)";
+
+            $stmt = mysqli_prepare($conn, $query);
+            mysqli_stmt_bind_param($stmt,"sss",
+            $name,$email,$message);
+                      
+            if(mysqli_stmt_execute($stmt)){
+                ;      
+            }else{
+                die("Insert Error".mysqli_error($conn));     
+            }
+
+            mysqli_stmt_close($stmt);
+            mysqli_close($conn);
+        }
     }
     ?>
 
